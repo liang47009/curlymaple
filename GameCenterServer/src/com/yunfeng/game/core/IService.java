3075fc80a45289bbfcc506c2bae65c27f6cc2ddd
xialiangliang <105144919@qq.com>
2013-05-07 15:41:14 +0800

    Uncomment

--- GameCenterServer/src/com/yunfeng/game/core/IService.java
+++ GameCenterServer/src/com/yunfeng/game/core/IService.java
@@ -11,8 +11,3 @@ public abstract interface IService {
 
 	public abstract void setName(String paramString);
 }
-
-/*
- * Location: D:\SmartFoxServer2X\SFS2X\lib\sfs2x-core.jar Qualified Name:
- * com.smartfoxserver.bitswarm.service.IService JD-Core Version: 0.6.2
- */
